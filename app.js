19fba6097091c3e7d8337496165ea4818733579e
Brecht <brechtnulens@gmail.com>
2019-11-19 11:54:04 +0100

    created basic navbar - ready to style with scss

--- app.js
+++ app.js
@@ -11,16 +11,21 @@ const app = express();
 
 const adminPath = require('./routes/admin');
 const shopPath = require('./routes/shop');
+const homePath = require('./routes/home');
 
 // Bodies have to be parsed before they land on any path
 app.use(bodyParser.urlencoded({extended: false}));
+
+// To serve files statically from the public folder 
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Outsourced routes 
 // Filtered route to the /admin
 app.use('/admin', adminPath);
 app.use(shopPath);
+app.use(homePath);
 
-// Adding a catch all middleware redirecting to a 404-page
+// Adding a 'catch all' middleware redirecting to a 404-page
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 });
--- routes/home.js
+++ routes/home.js
@@ -1,3 +1,4 @@
+// Path module is necessary to build the right filepath 
 const path = require('path');
 
 const express = require('express');
@@ -5,9 +6,9 @@ const router = express.Router();
 
 const rootDir = require('../utils/path');
 
-router.get('/shop', (req, res, next) => {
+router.get('/', (req, res, next) => {
     // join() method works on other OS as well, building a right path
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    res.sendFile(path.join(rootDir, 'views', 'home.html'));
 });
 
 module.exports = router;
