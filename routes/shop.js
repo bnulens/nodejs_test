e30701eeded8018b657ec82c27707b397d5135f6
Brecht <brechtnulens@gmail.com>
2019-11-20 17:19:11 +0100

    Preparing route to return EJS file/object

--- routes/shop.js
+++ routes/shop.js
@@ -4,10 +4,25 @@ const express = require('express');
 const router = express.Router();
 
 const rootDir = require('../utils/path');
+const adminData = require('./admin');
 
 router.get('/shop', (req, res, next) => {
+    
     // join() method works on other OS as well, building a right path
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    /*   Sending an HTML file as response 
+            res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    */
+    //  Sending an object as response
+    const products = adminData.products; 
+    res.render('shop', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/',
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
+    });
+
 });
 
 module.exports = router;
