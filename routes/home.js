60e2d4f209ca29bf723b55f553b910a407910356
Brecht <brechtnulens@gmail.com>
2019-11-21 17:03:22 +0100

    Return rendered homepage from template

--- routes/home.js
+++ routes/home.js
@@ -7,8 +7,14 @@ const router = express.Router();
 const rootDir = require('../utils/path');
 
 router.get('/', (req, res, next) => {
-    // join() method works on other OS as well, building a right path
-    res.sendFile(path.join(rootDir, 'views', 'home.html'));
+    /* join() method works on other OS as well, building a right path
+    Returning an HTML file as response
+     res.sendFile(path.join(rootDir, 'views', 'home.html'));
+    */
+    res.render('home', {
+        pageTitle: 'Homepage',
+        path: '/'
+    });
 });
 
 module.exports = router;
