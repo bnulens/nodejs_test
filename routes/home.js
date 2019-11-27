2b8df1c62431178325e210563191d15abce8fe6b
Brecht <brechtnulens@gmail.com>
2019-11-27 13:59:01 +0100

    MVC start - refactored home route to homeLand

--- routes/home.js
+++ routes/home.js
@@ -4,17 +4,10 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 
-const rootDir = require('../utils/path');
+const homeLand = require('../controllers/homepage');
 
-router.get('/', (req, res, next) => {
-    /* join() method works on other OS as well, building a right path
-    Returning an HTML file as response
-     res.sendFile(path.join(rootDir, 'views', 'home.html'));
-    */
-    res.render('home', {
-        pageTitle: 'Homepage',
-        path: '/'
-    });
-});
+// const rootDir = require('../utils/path');
+
+router.get('/', homeLand.homeController);
 
 module.exports = router;
