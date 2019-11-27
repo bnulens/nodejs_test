9ecd00a141b5cdc483b8370768de263f106d8c73
Brecht <brechtnulens@gmail.com>
2019-11-27 13:58:14 +0100

    MVC start - refactored shop route to productsController

--- routes/shop.js
+++ routes/shop.js
@@ -3,26 +3,8 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 
-const rootDir = require('../utils/path');
-const adminData = require('./admin');
+const productsController = require('../controllers/products');
 
-router.get('/shop', (req, res, next) => {
-    
-    // join() method works on other OS as well, building a right path
-    /*   Sending an HTML file as response 
-            res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-    */
-    //  Sending an object as response
-    const products = adminData.products; 
-    res.render('shop', {
-        prods: products,
-        pageTitle: 'Shop',
-        path: '/shop',
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
-    });
-
-});
+router.get('/shop', productsController.getProducts);
 
 module.exports = router;
