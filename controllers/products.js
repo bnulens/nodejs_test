40c617a3090fd1c456e1217ce5035480540e2919
Brecht <brechtnulens@gmail.com>
2019-11-27 14:00:42 +0100

    MVC start - new controller folder +  controller for products & shop

---
+++ controllers/products.js
@@ -0,0 +1,34 @@
+const products = [];
+
+exports.getAddProduct = (req, res, next) => {
+    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+    res.render('add-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
+    });
+};
+
+exports.postAddProduct = (req, res, next) => {
+    products.push({title: req.body.title});
+    res.redirect('/');
+}
+
+exports.getProducts = (req, res, next) => {
+    
+    // join() method works on other OS as well, building a right path
+    /*   Sending an HTML file as response 
+            res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    */
+    //  Sending an object as response
+    res.render('shop', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/shop',
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
+    });
+}
