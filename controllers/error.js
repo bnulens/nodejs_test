9e6026da440f7a96097ec1a5463ce28dc89d4dac
Brecht <brechtnulens@gmail.com>
2019-12-10 14:26:33 +0100

    Add error controller for 404 page

---
+++ controllers/error.js
@@ -0,0 +1,7 @@
+exports.get404 = (req, res, next) => {
+    /* Sending an HTML as a response, from a generated path 
+        res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    */
+    // Sending a response via EJS 
+    res.status(404).render('404', { pageTitle: 'Page Not Found' });
+};
