5713bb145bad3ab7ce68b797aac677ff9b1318b5
Brecht <brechtnulens@gmail.com>
2019-12-23 20:30:06 +0100

    Defined path for 404 page in render

--- controllers/error.js
+++ controllers/error.js
@@ -3,5 +3,5 @@ exports.get404 = (req, res, next) => {
         res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
     */
     // Sending a response via EJS 
-    res.status(404).render('404', { pageTitle: 'Page Not Found' });
+    res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
 };
