3f5e861761f62a395e521a440385e7c39d8f60d6
Brecht <brechtnulens@gmail.com>
2019-12-23 20:32:54 +0100

    Rename productsController => adminController - split logic for admin and products

--- routes/admin.js
+++ routes/admin.js
@@ -2,15 +2,17 @@ const express = require('express');
 
 const path = require('path');
 
-const productsController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
 // /admin/add-product => GET
 // Express stores this route and executes it when the request lands on it
-router.get('/add-product', productsController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
+
+router.get('/products', adminController.getAddProduct);
 
 // /admin/add-product => POST
-router.post('/add-product', productsController.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
