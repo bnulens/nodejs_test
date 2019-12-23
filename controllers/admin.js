9572f43e040e2b342b3c119eed058d9e3fe93b99
Brecht <brechtnulens@gmail.com>
2019-12-23 20:29:21 +0100

    Split admin controller from shop & products controllers

---
+++ controllers/admin.js
@@ -0,0 +1,26 @@
+const Product = require('../models/product');
+
+exports.getAddProduct = (req, res, next) => {
+    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+    res.render('admin/add-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product'
+    });
+};
+
+exports.postAddProduct = (req, res, next) => {
+    const product = new Product(req.body.title);
+    product.save();
+    res.redirect('/shop');
+};
+
+exports.getProducts = (req, res, next) => {
+    Product.fetchAll(products => {
+        res.render('admin/products', {
+            prods: products,
+            pageTitle: 'Admin Products',
+            path: '/admin/products'
+        });
+    });
+}
+
