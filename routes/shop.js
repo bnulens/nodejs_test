ef5d98d825b224436026f58d035ec8aea3848e77
Brecht <brechtnulens@gmail.com>
2019-12-30 21:07:08 +0100

    Router to use dynamic param for product ID

--- routes/shop.js
+++ routes/shop.js
@@ -9,10 +9,18 @@ router.use('/shop', shopController.getProducts);
 
 router.get('/products', shopController.getProducts);
 
+/* Dynamic parameters
+    -> product id is dynamically passed in as parameter
+    -> specific routes are to be put first 
+*/
+router.get('/products/:productId', shopController.getProduct);
+
 router.get('/cart', shopController.getCart);
 
 router.get('/checkout', shopController.getCheckout);
 
 router.get('/orders', shopController.getOrders);
+
+
 
 module.exports = router;
